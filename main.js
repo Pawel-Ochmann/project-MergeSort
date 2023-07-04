a6bb4ad66f9a93cdfbf4491b9bd632e0f1f1b44a
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-07-04 18:20:30 +0200

    The algorithm is working! Actually made it by accident, tested it manually, passed all tests by far

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-const testArray1 = [1, 3, 5];
-const testArray2 = [0, 2, 4];
+const testArray1 = [1];
 
 // function mergeSort(array) {
 
 // }
 
-function merge(a, b) {
-  let arraySorted = [];
-  while (a.length > 0 || b.length > 0) {
-    if (a[0] <= b[0] || b[0] === undefined) {
-      arraySorted.push(a.shift());
-    } else arraySorted.push(b.shift());
+function mergeSort(arr) {
+  function splitFirst(arr) {
+    if (arr.length === 1) return arr;
+    const firstHalf = arr.slice(0, Math.round(arr.length / 2));
+    return mergeSort(firstHalf);
   }
-  return arraySorted;
+
+  function splitSecond(arr) {
+    if (arr.length === 1) return arr;
+    const secondHalf = arr.slice(Math.round(arr.length / 2));
+    return mergeSort(secondHalf);
+  }
+  function merge(a, b) {
+    let arraySorted = [];
+    while (a.length > 0 || b.length > 0) {
+      if (a[0] <= b[0] || b[0] === undefined) {
+        arraySorted.push(a.shift());
+      } else arraySorted.push(b.shift());
+    }
+    return arraySorted;
+  }
+
+  return merge(splitFirst(arr), splitSecond(arr));
 }
 
-console.log(merge(testArray1, testArray2));
+console.log(mergeSort(testArray1));
